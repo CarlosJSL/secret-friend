1f0757e03cd0229d02976b2844e2dfeaf751efe0
Carlos José <carlosjsl95@gmail.com>
2019-12-03 23:27:12 -0300

    add contract service

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { abiSchema } from '../services';
 const Web3 = require('web3');
 
 function Home() {
@@ -27,6 +28,17 @@ function Home() {
       web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     let web3 = new Web3(web3Provider);
+    let contract = new web3.eth.Contract(abiSchema, '0x5C608BB56d94371d7eE5BAFf5ECe967D68613AEe');
+    doTransaction(contract);
+  }
+
+  async function doTransaction(contract) {
+    // const transaction = await contract.methods.getBalance().send({
+    //   from: '0x8d46Fa0ecAf7DE3E3d7E09dcc0153040F061FBDB',
+    //   value: 1,
+    // })
+    // console.log(transaction)
+    // const response = await contract.methods.getBalance().call({ from: '0x8d46Fa0ecAf7DE3E3d7E09dcc0153040F061FBDB' })
   }
 
   return (
---
+++ src/services.js
@@ -0,0 +1,97 @@
+export const abiSchema = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "new_user",
+        "type": "address"
+      }
+    ],
+    "name": "tranfer",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "withdraw",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getTax",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "total_days",
+        "type": "uint256"
+      }
+    ],
+    "name": "deposit",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getDaysRemaining",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+]
